10a5d2084fb495df0bb8524a6313756955082fde
SirWerto <sirwerto@gmail.com>
2023-01-27 22:59:53 +0100

    Fix Front server up while MIX_ENV=prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -28,7 +28,7 @@ if config_env() == :prod do
   # to check this value into version control, so we use an environment
   # variable instead.
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
+    System.get_env("MITRAVIAN__SECRET_KEY_BASE") ||
       raise """
       environment variable SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
@@ -41,7 +41,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    server: true
 
   # ## Using releases
   #
